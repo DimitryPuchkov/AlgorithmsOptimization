2d0cef6c2aff29883a76440c5cba258e8bafd6ab
Egor <lemonneon@mail.ru>
2021-10-09 15:02:31 +0700

    Task 3 were addewas(FINAL)

--- PZ4_Project/PZ4_Project/PZ4_Project.cpp
+++ PZ4_Project/PZ4_Project/PZ4_Project.cpp
@@ -1,4 +1,4 @@
-#include "mpi.h"
+﻿#include "mpi.h"
 #include <iostream>
 #include <windows.h>
 #include <algorithm>
@@ -6,10 +6,10 @@
 
 using namespace std;
 
-double* getMatrix(int n, int m)
+double *getMatrix(int n, int m)
 {
    int N = m * n;
-   double* a = NULL;
+   double *a = NULL;
 
    if (a == NULL)
       a = new double[N];
@@ -22,7 +22,7 @@ double* getMatrix(int n, int m)
    return a;
 }
 
-double norma(double* A, int n, int m)
+double norma(double *A, int n, int m)
 {
    double res = 0;
    long long i, j;
@@ -33,7 +33,7 @@ double norma(double* A, int n, int m)
    return sqrt(res);
 }
 
-double parallel_norma(double* A, int n, int m)
+double parallel_norma(double *A, int n, int m)
 {
    double res = 0;
    long long i, j;
@@ -47,7 +47,7 @@ double parallel_norma(double* A, int n, int m)
 
 
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 
    int rank, size;
@@ -59,39 +59,37 @@ int main(int argc, char** argv)
    MPI_Comm_size(MPI_COMM_WORLD, &size);
    if (rank > 1)
    {
-      double* v = getMatrix(1, 100000);
+      double *v = getMatrix(1, 100000);
       double n = norma(v, 1, 100000);
       MPI_Send(&n, 1, MPI_DOUBLE, 0, 100, MPI_COMM_WORLD);
       cout << "Sended from " << rank << endl;
    }
    if (rank == 1) {
-      double* v = getMatrix(1, 100000);
+      double *v = getMatrix(1, 100000);
       double n = norma(v, 1, 100000);
-      Sleep(10000); // delay
+      Sleep(10000); 
       MPI_Send(&n, 1, MPI_DOUBLE, 0, 100, MPI_COMM_WORLD);
       cout << "Sended from " << rank << endl;
    }
-  
-   else { // Master
-      double n[9]= {0};
-      //int flag[9] = {0};
+
+   if(rank == 0) { 
+      double n[9] = { 0 };
       array<int, 9> flag = { 0 };
       bool w = true;
       MPI_Request request[9];
       MPI_Status status;
       for (int i = 1; i < size; i++)
-         MPI_Irecv(&n[i-1], 1, MPI_DOUBLE, i, 100, MPI_COMM_WORLD, &request[i-1]);
-      //Sleep(90000); //sleep 10sec
+         MPI_Irecv(&n[i - 1], 1, MPI_DOUBLE, i, 100, MPI_COMM_WORLD, &request[i - 1]);
 
-      while (!all_of(flag.begin(), flag.end(), [](int i) {return i == 1; })){
-         for (int i = 1; i < size; i++)
-            MPI_Test(&request[i - 1], &flag[i-1], &status);
-      }
-      for (int i = 1; i < 9; i++)
+     while (!all_of(flag.begin(), flag.end(), [](int i) {return i == 1; }))
+        for (int i = 1; i < size; i++)
+           MPI_Test(&request[i - 1], &flag[i - 1], &status);
+
+      for (int i = 1; i < size; i++)
          cout << "recived from" << i << " norma: " << n[i - 1] << endl;
-      //cout<<1;
+        
    }
-   //cout<<"ths";
+
    MPI_Finalize();
    return 0;
 }
