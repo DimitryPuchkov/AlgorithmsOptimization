c226c47a14a954eb4e9203c8097427306a999344
Dmitry <puchkovdmitrybor@gmail.com>
2021-10-09 15:13:44 +0700

    updated pz5

--- PZ5/PZ5/PZ5.cpp
+++ PZ5/PZ5/PZ5.cpp
@@ -1,20 +1,14 @@
-// PZ5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include "mpi.h"
 #include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+   int rank, size;
+   MPI_Status status;
+   /* Init */
+   MPI_Init(&argc, &argv);
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+   return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
